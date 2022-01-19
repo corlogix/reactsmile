af46a23276442479026d23c96bd32d185517e855
corlogix <corlogix@gmail.com>
2022-01-19 10:11:01 -0500

    fix: icons src

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -1,52 +1,62 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Icon = void 0;
-const react_1 = require("react");
-const icons = [
-    'alarm',
-    'bag',
-    'bank',
-    'box',
-    'calendar',
-    'card',
-    'chart-alt',
-    'chart',
-    'clock',
-    'filter',
-    'flask-alt',
-    'flask',
-    'home',
-    'medkit',
-    'moon',
-    'palette',
-    'pulse',
-    'shopping-bag',
-    'shopping-basket',
-    'shopping-cart',
-    'sun',
-    'timer',
-    'umbrella',
-    'wallet'
-];
-const classNames = (...classNames) => classNames
-    .filter(x => {
-    if (typeof x === "object") {
-        const key = Object.keys(x)[0];
-        console.log({ x }, { key, test: x[key] });
-        return x[key];
-    }
-    return x;
-}).map(x => {
-    if (typeof x === "object") {
-        return Object.keys(x)[0];
-    }
-    return x;
-})
-    .join(" ");
-function Icon({ name, outline = false, className, ...props }) {
-    const classes = classNames('rs-icon', { [`rs-${name}`]: !outline }, { [`rs-${name}-o`]: outline }, className);
-    console.log({ classes });
-    return <span className={classes} {...props}/>;
+require("regenerator-runtime/runtime");
+
+const fs = require("fs"), path = require("path");
+const generator = require("@vusion/webfonts-generator");
+const rimraf = require("rimraf");
+const createReactComponent = require("../templates/react-component");
+
+
+const input = path.resolve("svgs");
+const output = path.resolve("styles");
+const componentPath = path.join(path.resolve("src"), "index.tsx");
+const templates = path.resolve("templates");
+
+console.log({ input, output, templates, componentPath });
+
+function toLower(x = "") {
+    return x.toLowerCase();
 }
-exports.Icon = Icon;
-exports.default = Icon;
+
+(async () => {
+
+    const clean = () => new Promise(resolve => {
+        rimraf.sync(componentPath);
+        rimraf.sync(output);
+        fs.mkdirSync(output);
+        resolve();
+    })
+
+    await clean();
+
+    const SVGs = fs.readdirSync(input).map(toLower).filter(x => x.endsWith(".svg"));
+
+    console.log("\nFound", SVGs.length, "SVG files to process");
+
+    generator({
+        files: SVGs.map(svg => path.join(input, svg)),
+        dest: output,
+        html: true,
+        fontName: "reactsmile-iconography",
+        cssTemplate: path.join(templates, "css.hbs"),
+        templateOptions: {
+            classPrefix: "rs-",
+            baseSelector: ".rs-icon"
+        },
+        types: ['woff2', 'woff', 'eot', 'ttf', 'svg'],
+        normalize: false
+    }, function(error, result) {
+        if(error) {
+            throw Error(error);
+        } else {
+            result.generateHtml();
+            console.log("\nFonts generated!");
+        }
+    })
+
+    try {
+        fs.writeFileSync(componentPath, createReactComponent("rs", SVGs));
+        console.log("Typescript file created!")
+    } catch(err) {
+        throw Error(err);
+    }
+})();
