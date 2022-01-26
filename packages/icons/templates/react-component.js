93935437bff86cd1b1f7cc6b47c00fc6b9855a94
corlogix <corlogix@gmail.com>
2022-01-25 19:32:00 -0500

    fix: export all icons

--- packages/icons/templates/react-component.js
+++ packages/icons/templates/react-component.js
@@ -1,12 +1,16 @@
 module.exports = (prefix = "rs", icons = []) => `
 import React from "react";
 
-const icons = [
+export const ReactSmileIconsArray = [
+    ${icons.map(icon => `'${icon.replace(".svg", "")}'`).join(",\n\t")}
+];
+
+export const ReactSmileIcons = [
     ${icons.map(icon => `'${icon.replace(".svg", "")}'`).filter(x => !x.endsWith("-o'")).join(",\n\t")}
 ] as const;
 
 export type IconProps = React.HTMLAttributes<HTMLSpanElement> & {
-    name: typeof icons[number];
+    name: typeof ReactSmileIcons[number];
     outline?: boolean;
 }
 
